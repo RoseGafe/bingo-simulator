1e8200f3be047f04a22e6fd84355273d61db8cd4
Sofía Rojas <sofiarojasruocco@gmail.com>
2022-01-11 22:49:45 +0100

    function showCard defined but not working

--- src/index.js
+++ src/index.js
@@ -5,22 +5,30 @@ const btn = document.getElementById("btn"); //const btn1 = document.querySelecto
 const array = _.range(1, 91);
 
 let randomArr = _.shuffle(array);
-console.log(randomArr); //había pensado en hacer un shuffle, de ahí coger los 15 primeros números para player, repetir otro shuffle y coger los 15 del pc
-let numb = randomArr[0];
-console.log('es '+ numb);
-
-// *! borrad los comentarios que queráis, que he ido divagando un poco
-
-//let playerCard = document.querySelector("player");
-//const div = createElement("div");
-//div.textContent = numb;
-
-//playerCard.className = "player-card";
-
-let card = document.createElement("div");
-card.className = `number number-${numb}`; //no sé si esto valdría para poner el número en la clase
+const pcCard = randomArr.slice(0, 15);
+console.log(pcCard); //había pensado en hacer un shuffle, de ahí coger los 15 primeros números para player, repetir otro shuffle y coger los 15 del pc
+randomArr = _.shuffle(array);
+const playerCard = randomArr.slice(0, 15);
+console.log(playerCard);
+// Con esto tenemos el carton del jugador y el del pc (en array)
 
 //*TODO function showCard(element, card){}
+function showCard ( element, card) {
+    let newCard = document.querySelector(`.${element}`);
+
+    for(let i of card){
+        let divCard = document.createElement('div');
+        divCard.className = `number number-${i}`;
+        console.log(i);
+         divCard.textContent = i;
+        newCard.appendChild(divCard);
+    }
+
+} // no he conseguido que me funcione, me dice:
+// Cannot read properties of null (reading 'appendChild')
+
+showCard('playerCard', playerCard);
+showCard('pcCard', pcCard);
 
 function newNumber(){
     let pos = Math.floor(Math.random() * 90);
